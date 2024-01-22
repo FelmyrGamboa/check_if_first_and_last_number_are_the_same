7fb67a8a61c7b9e36114a6e36fd128d8b03f6c44
FelmyrGamboa <felmyrgamboa23@gmail.com>
2024-01-22 16:27:37 +0800

    Pseudocodes

--- checking_the_first_and_last_number_of_a_list_if_same.py
+++ checking_the_first_and_last_number_of_a_list_if_same.py
@@ -1,2 +1,19 @@
 # Exercise 5: Check if the first and last number of a list is the same
 # Write a function to return True if the first and last number of a given list is same. If numbers are different then return False.
+
+# Given:
+
+# numbers_x = [10, 20, 30, 40, 10]
+# numbers_y = [75, 65, 35, 75, 30]
+
+#Create a function to check the first and last number
+
+#Write a condition to return if first and last number of the list is same
+
+#If not return false
+
+#Give the list
+
+#Call out the function
+
+#Display the result
