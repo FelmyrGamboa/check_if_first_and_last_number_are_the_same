b5cc0d9fe20c1f16295109ce17b3be842ef4c137
FelmyrGamboa <felmyrgamboa23@gmail.com>
2024-01-22 16:30:26 +0800

    Completed creating the function with the conditions in checking the numbers

--- checking_the_first_and_last_number_of_a_list_if_same.py
+++ checking_the_first_and_last_number_of_a_list_if_same.py
@@ -7,11 +7,14 @@
 # numbers_y = [75, 65, 35, 75, 30]
 
 #Create a function to check the first and last number
-
+def check_first_and_last_number(number_list):
 #Write a condition to return if first and last number of the list is same
-
+    if number_list[0] == number_list[4]:
+        return True
 #If not return false
-
+    else:
+        return False
+    
 #Give the list
 
 #Call out the function
